3dd0514724d8feafabcc154fe35e5d39b4ecfbe8
YourDad07 <salmanadhikari415@gmail.com>
2022-04-25 10:51:04 +0530

    Handeling the exceptions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file
+from flask import Flask, redirect, send_file
 from flask import render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
@@ -66,88 +66,108 @@ def Log_in():
 @app.route('/uploadimg',methods=['GET','POST'])
 def uploadimg():
     if request.method == 'POST':
-        file = request.files['file']
-        file.save(os.path.join('OCR/images', file.filename))
-        import json
-        with open('actual_res.json', encoding='utf-8') as f:
-            data = json.load(f)
-        data = data['textAnnotations']
-        data = over_all(data)
-        return render_template('ocrcontain.html',datas = data)
+        try:
+            file = request.files['file']
+            file.save(os.path.join('OCR/images', file.filename))
+            import json
+            with open('actual_res.json', encoding='utf-8') as f:
+                data = json.load(f)
+            data = data['textAnnotations']
+            data = over_all(data)
+            return render_template('ocrcontain.html',datas = data)
+        except Exception as e:
+            print(e)
+            return render_template('dashboard.html')
     else:
         return render_template('dashboard.html')
 
 @app.route("/submit", methods = ['GET', 'POST'])
 def submit():
     if request.method =='POST':
-        print("--->>  ")
-        count = int(request.form["count"])
-        subject_name = request.form["subname"]
-        collegename = request.form["collegename"]
-        print(count)
-        for i in range(1,count):
-            PRN = request.form['prn'+str(i)]
-            Name = request.form['name'+str(i)]
-            try:
-                Mcq_marks = int(request.form['mcq'+str(i)])
-                q2_marks =  int(request.form['q2'+str(i)])
-                q3_marks = int(request.form['q3'+str(i)])
-            except Exception as e:
-                Mcq_marks = 0
-                q2_marks = 0
-                q3_marks = 0
-            print(PRN,Name,Mcq_marks,q2_marks,q3_marks)
-            Results = Result(seat_no=PRN, name=Name, Mcq_marks=Mcq_marks,q2_marks=q2_marks,q3_marks=q3_marks,Tot_des_marks=q2_marks+q3_marks,Tot_marks=q2_marks+q3_marks+Mcq_marks,subject=subject_name,college_name=collegename)
-            db.session.add(Results)
-            db.session.commit()
-        return "work"
+        try:
+            print("--->>  ")
+            count = int(request.form["count"])
+            subject_name = request.form["subname"]
+            collegename = request.form["collegename"]
+            print(count)
+            for i in range(1,count):
+                PRN = request.form['prn'+str(i)]
+                Name = request.form['name'+str(i)]
+                try:
+                    Mcq_marks = int(request.form['mcq'+str(i)])
+                    q2_marks =  int(request.form['q2'+str(i)])
+                    q3_marks = int(request.form['q3'+str(i)])
+                except Exception as e:
+                    Mcq_marks = 0
+                    q2_marks = 0
+                    q3_marks = 0
+                print(PRN,Name,Mcq_marks,q2_marks,q3_marks)
+                Results = Result(seat_no=PRN, name=Name, Mcq_marks=Mcq_marks,q2_marks=q2_marks,q3_marks=q3_marks,Tot_des_marks=q2_marks+q3_marks,Tot_marks=q2_marks+q3_marks+Mcq_marks,subject=subject_name,college_name=collegename)
+                db.session.add(Results)
+                db.session.commit()
+                flash("Successfully added")
+        except Exception as e:
+            print(e)
+            flash("Error")
+        return render_template("dashboard.html")
         
-    return "o"
+    return render_template('dashboard.html')
 
 @app.route('/result',methods=['GET', 'POST'])
 def result():
     if request.method == 'POST':
-        seat_no = request.form["seat_no"]
-        # full_name = request.form["full_name"]
-        # print(seat_no,full_name)
-        data = list(db.session.execute(f"""select "subject","Mcq_marks","q2_marks","q3_marks","Tot_des_marks","Tot_marks" from result where seat_no ='{seat_no}'""").all())
-        info = list(db.session.execute(f"""select name, seat_no,college_name from result where seat_no ='{seat_no}'""").first())
-        print(info)
-        new_data2 = list()
-        new_data = list()
-        name_list = ["Subject","Mcq Marks","Q2 Marks","Q3 Marks","Total Descriptive Marks","Total Marks"]
-        new_data.append(name_list)
-        for d in data:
-            new_data2.append(list(d))
-            new_data.append(list(d))
+        try:
+            seat_no = request.form["seat_no"]
+            # full_name = request.form["full_name"]
+            # print(seat_no,full_name)
+            data = list(db.session.execute(f"""select "subject","Mcq_marks","q2_marks","q3_marks","Tot_des_marks","Tot_marks" from result where seat_no ='{seat_no}'""").all())
+            info = list(db.session.execute(f"""select name, seat_no,college_name from result where seat_no ='{seat_no}'""").first())
+            print(info)
+            new_data2 = list()
+            new_data = list()
+            name_list = ["Subject","Mcq Marks","Q2 Marks","Q3 Marks","Total Descriptive Marks","Total Marks"]
+            new_data.append(name_list)
+            for d in data:
+                new_data2.append(list(d))
+                new_data.append(list(d))
 
-        print('---------------- >>>>>>>>>>>>> ',new_data)
-        sendPdf(new_data,info)
-        return render_template('result.html',datas=new_data2,info=info)
+            print('---------------- >>>>>>>>>>>>> ',new_data)
+            sendPdf(new_data,info)
+            return render_template('result.html',datas=new_data2,info=info)
+        except Exception as e:
+            print(e)
+            flash("Error")
+            return render_template('index.html')
         # return render_template('result.html',data=data)
     else:
-        return render_template('dashboard.html')
+        return render_template('index.html')
 
-@app.route('/download',methods=['GET', 'POST'])
+@app.route('/download',methods=['POST'])
 def download():
     if request.method == 'POST':
-        seat_no = request.form["seat_no"]
-        # full_name = request.form["full_name"]
-        # print(seat_no,full_name)
-        data = list(db.session.execute(f"""select "subject","Mcq_marks","q2_marks","q3_marks","Tot_des_marks","Tot_marks" from result where seat_no ='{seat_no}'""").all())
-        info = list(db.session.execute(f"""select name, seat_no,college_name from result where seat_no ='{seat_no}'""").first())
-        print(info)
-        new_data2 = list()
-        new_data = list()
-        name_list = ["Subject","Mcq Marks","Q2 Marks","Q3 Marks","Total Descriptive Marks","Total Marks"]
-        new_data.append(name_list)
-        for d in data:
-            new_data2.append(list(d))
-            new_data.append(list(d))
+        try:
+            seat_no = request.form["seat_no"]
+            # full_name = request.form["full_name"]
+            # print(seat_no,full_name)
+            data = list(db.session.execute(f"""select "subject","Mcq_marks","q2_marks","q3_marks","Tot_des_marks","Tot_marks" from result where seat_no ='{seat_no}'""").all())
+            info = list(db.session.execute(f"""select name, seat_no,college_name from result where seat_no ='{seat_no}'""").first())
+            print(info)
+            new_data2 = list()
+            new_data = list()
+            name_list = ["Subject","Mcq Marks","Q2 Marks","Q3 Marks","Total Descriptive Marks","Total Marks"]
+            new_data.append(name_list)
+            for d in data:
+                new_data2.append(list(d))
+                new_data.append(list(d))
 
-        print('---------------- >>>>>>>>>>>>> ',new_data)
-        path = sendPdf(new_data,info)
-        return send_file(path,as_attachment=True)
+            print('---------------- >>>>>>>>>>>>> ',new_data)
+            path = sendPdf(new_data,info)
+            return send_file(path,as_attachment=True)
+        except Exception as e:
+            print(e)
+            flash("Error")
+            return render_template('index.html')
+
         # return render_template('result.html',data=data)
 if __name__ == '__main__':
     app.run(debug=True)
