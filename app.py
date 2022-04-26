814a70aa879cb78c609b7a7a000538011af14e85
YourDad07 <salmanadhikari415@gmail.com>
2022-04-26 20:54:48 +0530

    Middleware : Improve The Folder Strcuture

--- app.py
+++ app.py
@@ -1,33 +1,25 @@
 from flask import Flask
 from flask import render_template
-from flask_login import LoginManager, login_manager, AnonymousUserMixin
 from dotenv import load_dotenv
 import os
-app = Flask(__name__)
-load_dotenv('.env')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:test123@localhost:5433/OCR"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:test1234@localhost:5433/OCR"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['JSON_AS_ASCII'] = False
 from models import db
-db.init_app(app)
-from models.Admin import admin
+from middleware import login_manager
 from routes.uploadimg import uploadimg
 from routes.login import login
 from routes.submit import submit
 from routes.result import result
 from routes.download import download
 from routes.logout import logout
-login_manager = LoginManager()
+app = Flask(__name__)
+load_dotenv('.env')
+from middleware.auth import *
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:test123@localhost:5433/OCR"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:test1234@localhost:5433/OCR"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+db.init_app(app)
 login_manager.init_app(app)
-login_manager.login_view = 'login.logins'
-class Anonymous(AnonymousUserMixin):
-    def __init__(self):
-        self.username = 'Guest'
-@login_manager.user_loader
-def load_user(user_id):
-    return admin.query.get(int(user_id))
 app.register_blueprint(uploadimg,url_prefix='/uploadimg')
 app.register_blueprint(login,url_prefix='/login')
 app.register_blueprint(submit,url_prefix='/submit')
